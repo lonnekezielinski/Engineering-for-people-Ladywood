d38b75c1f804155e063a611f66e0c7ff70a19920
Lonneke Zielinski <l.n.zielinski@student.tue.nl>
2026-05-31 17:36:44 +0200

    put in correct logo on main page

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-logo_icon = get_base64_image("assets/Waypoint.png")
+logo_icon = get_base64_image("assets/waypoint-logo.png")
 bus_icon          = get_base64_image("assets/front-of-bus.png")
 workshop_icon     = get_base64_image("assets/calendar.png")
 announcement_icon = get_base64_image("assets/megaphone.png")
